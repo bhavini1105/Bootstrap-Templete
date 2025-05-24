3569fa6ba084f72222bdec08a79be01cfeb05e5c
bhavini1105 <bhavinipatel7532@gmail.com>
2025-05-24 21:33:04 +0530

    Bootstrap-Template

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Components/Home'
 import Fruit from './Components/Fruit'
 import Service from './Components/Service'
 import Contact from './Components/Contact'
+import "../public/css/style.css"
 
 function App() {
   return (
@@ -12,10 +13,10 @@ function App() {
 
     <Header/>
     <Routes>
-      <Route element={<Home/>}></Route>
-      <Route element={<Fruit/>}></Route>
-      <Route element={<Service/>}></Route>
-      <Route element={<Contact/>}></Route>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/fruit' element={<Fruit/>}></Route>
+      <Route path='/service' element={<Service/>}></Route>
+      <Route path='/contact' element={<Contact/>}></Route>
     </Routes>
       
     </>
